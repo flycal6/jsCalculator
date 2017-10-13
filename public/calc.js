0cd06c9e541f2ce58b9b627fdb259fcb5e9275d0
Brian Thomas <brianraythomas@gmail.com>
2017-10-13 17:19:16 -0600

    all basic math functions and 'clear' work

---
+++ public/calc.js
@@ -0,0 +1,93 @@
+$('.number').click(function(e) {
+    var currentText = $('#display').text();
+    $('#display').text(currentText + $(this).text());
+});
+
+var num1;
+var num2;
+var operation;
+$('.operator').click(function(e) {
+    var oper = $(this).text();
+    switch (oper) {
+        case '/':
+            num1 = $('#display').text();
+            clearDisplay();
+            operation = '/';
+            break;
+
+        case 'X':
+            num1 = $('#display').text();
+            clearDisplay();
+            operation = 'X';
+            break;
+
+        case '+':
+            num1 = $('#display').text();
+            clearDisplay();
+            operation = '+';
+            break;
+
+        case '-':
+            num1 = $('#display').text();
+            clearDisplay();
+            operation = '-';
+
+            break;
+        case '=':
+            num2 = $('#display').text();
+            controller(operation);
+            break;
+
+        case 'C':
+            clearDisplay();
+            operation = '';
+            num1 = 0;
+            num2 = 0;
+            break;
+        default:
+    }
+
+});
+
+function controller(operator) {
+    switch (operator) {
+        case '/':
+            divide(num1, num2);
+
+            break;
+        case 'X':
+            multiply(num1, num2);
+            break;
+        case '+':
+            sum(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+    }
+}
+
+function divide(num1, num2) {
+    clearDisplay();
+    $('#display').text(num1 / num2);
+}
+
+function multiply(num1, num2) {
+    clearDisplay();
+    $('#display').text(num1 * num2);
+}
+
+function subtract(num1, num2) {
+    clearDisplay();
+    $('#display').text(num1 - num2);
+}
+
+function sum(num1, num2) {
+    clearDisplay();
+    $('#display').text(parseInt(num1) + parseInt(num2));
+    operation = '';
+}
+
+function clearDisplay() {
+    $('#display').text('');
+}
